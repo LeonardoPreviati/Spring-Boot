0957521f9ad17f11f5ca1ddb2631d4fb32f47f9c
kleber barilli <kleber.barilli@compasso.com.br>
2020-05-15 17:51:48 -0300

    habilitando o cors para comunicação local

--- src/main/java/br/com/empreendedorismo/ApiEmpreendendorismoApplication.java
+++ src/main/java/br/com/empreendedorismo/ApiEmpreendendorismoApplication.java
@@ -1,5 +1,5 @@
 package br.com.empreendedorismo;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +10,9 @@ public class ApiEmpreendendorismoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiEmpreendendorismoApplication.class, args);
 		
+	}
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET","POST");
 	}
 
 }
