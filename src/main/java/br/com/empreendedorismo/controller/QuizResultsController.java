19c917f418774c230ee6f8f08c2fff440d793389
LeonardoPreviati <leonardo.previati@hotmail.com>
2020-05-28 04:02:51 -0300

    insert status code and verify account_id valid

--- src/main/java/br/com/empreendedorismo/controller/QuizResultsController.java
+++ src/main/java/br/com/empreendedorismo/controller/QuizResultsController.java
@@ -2,6 +2,7 @@ package br.com.empreendedorismo.controller;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.empreendedorismo.entity.QuizResults;
+import br.com.empreendedorismo.respository.AccountRepository;
+import br.com.empreendedorismo.respository.QuizResultsRepository;
 import br.com.empreendedorismo.service.QuizResultsService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,16 +28,26 @@ public class QuizResultsController {
 	@Autowired
 	private QuizResultsService quizResultsService;
 	
+	@Autowired
+	private AccountRepository accountRepository;
+	
 	@RequestMapping(value = "{accountId}", method = RequestMethod.GET)
-	public HashMap<String, Double> findResultsById(@PathVariable Integer accountId) throws Exception{
+	public ResponseEntity<HashMap<String, Double>> findResultsById(@PathVariable Integer accountId) throws Exception{
 		long startTime = System.currentTimeMillis();
 		log.info("QuizResultsController.findResultsById() - BEGIN");
-		HashMap<String, Double> listQuizResults = null;
+		ResponseEntity<HashMap<String, Double>> listQuizResults = null;
+		Optional<Account> optional = accountRepository.findById(accountId);
 		try {
-			listQuizResults = quizResultsService.findResultsById(accountId);
-			
+			if(optional.isPresent()) {
+				listQuizResults = ResponseEntity.ok(quizResultsService.findResultsById(accountId));
+			}if(optional.isPresent() && quizResultsService.findResultsById(accountId).isEmpty()) {
+				listQuizResults = ResponseEntity.noContent().build();
+			}if (!optional.isPresent()){
+				listQuizResults = ResponseEntity.notFound().build();
+			}
 		}catch (Exception e) {
 			e.printStackTrace();
+			log.error("ERROR: " + e.getMessage());
 			throw e;
 		}
 		long endTime = System.currentTimeMillis() - startTime;
